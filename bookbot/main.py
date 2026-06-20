611ed502eaff43e38e84a2d1ea59c206cfaaf19e
Umbra <umbra.codex1@gmail.com>
2026-06-19 20:33:40 -0400

    018: updated bookbot

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,28 +1,36 @@
 import sys
-from stats import calculate_number_of_words, calculate_number_of_characters, sort_number_of_characters_into_list
+
+from stats import (
+    calculate_number_of_characters,
+    calculate_number_of_words,
+    chars_dict_to_sorted_list,
+)
+
 
 def get_book_text(path_to_file):
-  with open(path_to_file) as file:
-    file_contents = file.read()
-  return file_contents
+    with open(path_to_file) as file:
+        file_contents = file.read()
+    return file_contents
+
 
 def main():
-  if len(sys.argv) == 1:
-    print("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-  path = sys.argv[1]
-  book = get_book_text(path)
-  word_count = calculate_number_of_words(book)
-  character_count = calculate_number_of_characters(book)
-  sorted_by_character_count = sort_number_of_characters_into_list(character_count)
-  
-  print("============ BOOKBOT ============")
-  print(f"Analyzing book found at {path}...")
-  print("----------- Word Count ----------")
-  print(f"Found {word_count} total words")
-  for items in sorted_by_character_count:
-    print(f"{items["char"]}: {items["num"]}")
-  print("============= END ===============")
+    path = sys.argv[1]
+    book = get_book_text(path)
+    word_count = calculate_number_of_words(book)
+    character_count = calculate_number_of_characters(book)
+    sorted_list = chars_dict_to_sorted_list(character_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    for item in sorted_list:
+        print(f"('{item[0]}', {item[1]})")
+    print("============= END ===============")
+
 
 main()
--- bookbot/stats.py
+++ bookbot/stats.py
@@ -1,27 +1,27 @@
 def calculate_number_of_words(book):
-  separated_text = book.split()
-  count = 0
-  for word in separated_text:
-    count += 1
-  return count
+    separated_text = book.split()
+    count = 0
+    for text in separated_text:
+        count += 1
+    return count
 
 def calculate_number_of_characters(book):
-  separated_text = book.lower()
-  char_count = {}
-  for char in separated_text:
-    if char not in char_count:
-      char_count[char] = 1
-    elif char in char_count:
-      char_count[char] += 1
-  return char_count
+    separated_text = book.lower()
+    char_count = {}
+    for char in separated_text:
+        if char not in char_count:
+            char_count[char] = 1
+        elif char in char_count:
+            char_count[char] += 1
+    return char_count
 
-def sort_number_of_characters_into_list(dictionary):
+def chars_dict_to_sorted_list(chars_dict):
   sorted_list = []
-  for char in dictionary:
-    if char.isalpha() == True:
-      sorted_list.append({"char": char, "num": dictionary[char]})
-  sorted_list.sort(key=sort_on, reverse=True)
+  for char in chars_dict:
+    if char.isalpha():
+      sorted_list.append((char, chars_dict[char]))
+  sorted_list = sorted(sorted_list, key=sort_on, reverse=True)
   return sorted_list
 
-def sort_on(items):
-  return items["num"]
+def sort_on(items: tuple[str, int]) -> int:
+    return items[1]
