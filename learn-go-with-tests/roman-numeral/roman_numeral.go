184279722f10832933f64918bd70fe8c2676ae10
Umbra <umbra.codex1@gmail.com>
2026-08-28 02:51:48 -0400

    047: added roman-numeral to learn-go-with-tests dir

---
+++ learn-go-with-tests/roman-numeral/roman_numeral.go
@@ -0,0 +1,51 @@
+package v1
+
+import "strings"
+
+type RomanNumeral struct {
+	Value  uint16
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{49, "XLIX"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic uint16) string {
+	var roman strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			roman.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return roman.String()
+}
+
+func ConvertToArabic(roman string) uint16 {
+	var arabic uint16 = 0
+
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += uint16(numeral.Value)
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+
+	return arabic
+}
