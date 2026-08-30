6dea62fb4503b954a48b59e1511d60cd002a4b53
Umbra <umbra.codex1@gmail.com>
2026-08-30 02:10:33 -0400

    050: updated repl.go and added main.go to bootdev-pokedex dir

--- bootdev-pokedex/repl.go
+++ bootdev-pokedex/repl.go
@@ -1,6 +1,27 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		fmt.Printf("Your command was: %s\n", words[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	lower := strings.ToLower(text)
